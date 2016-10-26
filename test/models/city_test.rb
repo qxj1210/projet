2cb1c3c6c5c268f1876398664627340629e7b9b6
heleneqiu <heleneqiu@163.com>
2016-10-26 18:08:53 -0400

    hero

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,17 +1,11 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
- setup do
-   @city = cities(:one)
-   @cityWrong = cities(:two)
- end
- 
- test "should create city" do
-   VCR.use_cassette("nominatim") do
-   assert_difference('City.count') do
-     city = City.new  name: @city.name
-     city.save
-      end
-     end
-   end
+  
+  test "valid city" do
+    VCR.use_cassette("nominatim1") do
+      incorrect = City.new name: cities(:one).name
+      assert incorrect.valid?
+    end
+  end  
 end
