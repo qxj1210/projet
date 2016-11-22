03c61174321b26fee1280738dd0ee1934dc98f5c
heleneqiu <heleneqiu@163.com>
2016-11-22 08:25:07 -0500

    travis

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ ruby '2.3.1'
 gem 'rails', '4.1.6'
 
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.3'
+gem 'sass-rails', '>= 3.2'
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
@@ -30,8 +31,10 @@ group :development, :test do
   gem 'vcr', '~> 3.0', '>= 3.0.3'
 end
 
+
 # geocoding 
 gem 'nominatim', '~> 0.0.5'
+
 
 gem 'pg', '~> 0.18.4', group: :production
 
--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  
-  test "valid city" do
-    VCR.use_cassette("nominatim1") do
-      incorrect = City.new name: cities(:one).name
-      assert incorrect.valid?
-    end
-  end  
+ setup do
+   @city = cities(:one)
+   @cityWrong = cities(:two)
+ end
+ 
+ test "should create city" do
+   VCR.use_cassette("nominatim") do
+   assert_difference('City.count') do
+     city = City.new  name: @city.name
+     city.save
+      end
+     end
+  end
+
+  test 'weather forecast' do 
+     VCR.use_cassette("weather") do
+       weather = cities(:one).weather
+       assert weather=='rain'
+     end
+  end
 end
